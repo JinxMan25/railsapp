b0cae51af7556799229633f91ca0c7ba2f4bf40b
Md Samiul Huq <samiulg3@gmail.com>
2014-06-01 13:44:19 +1000

    Added model and posts edit/show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,36 @@ class PostsController < ApplicationController
       format.json { render :json => @posts }
     end
   end
+
+  def new
+    @post = Post.new
+    respond_to do |format|
+      format.html
+      format.json { render :json => @post }
+    end
+  end
+
+
+  def create
+    @post = Post.new(params[:post])
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to(posts_path, :notice => 'Post succesfully created' ) }
+        format.json { render:json => @post, :status => :created, :location => :post }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @post.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+
+    respond_to do |format|
+      format.html 
+      format.json { render :json => @post }
+    end
+  end
 end
