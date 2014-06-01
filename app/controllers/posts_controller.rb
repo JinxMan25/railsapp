10ca39f0009e732f13efe437806188faecf30437
Md Samiul Huq <samiulg3@gmail.com>
2014-06-01 13:34:24 +1000

    added index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @posts }
+    end
   end
+end
