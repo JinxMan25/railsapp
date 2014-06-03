8f9fc436d3372b32d35198c1e0d2045af2d92689
Md Samiul Huq <samiulg3@gmail.com>
2014-06-03 14:36:37 +1000

    Added validation for models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :author, :body, :title
+  validates :author, presence: true
+  validates :body, length: { maximum: 400, too_long: "Sami -_-, max length is %{counts}" }
+  validates :title, presence:true, length: { maximum: 60, too_long: "Sami -_-, max length is %{counts}" }
+
 end
